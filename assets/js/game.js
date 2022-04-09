af09a139a670be622802304cf933b849a19a506b
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-09 14:54:33 -0700

    added shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,6 +73,55 @@ var fight = function(enemyName) {
     function endGame(){
       window.alert("Thank you for playing")
     }
+    var shop = function(){
+
+      var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      );
+
+      // use switch to carry out action
+switch (shopOptionPrompt) {
+  case "refill":
+  case "REFILL":
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else {
+        window.alert("You don't have enough money!");
+      }
+       break;
+  case "upgrade":
+  case "UPGRADE":
+    if (playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+  
+     // increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+    break;
+  case "leave":
+  case "LEAVE":
+    window.alert("Leaving the store.");
+
+    // do nothing, so function will end
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+
+    // call shop() again to force player to pick a valid option
+    shop();
+    break;
+}
+
+    }
 
     function resetGame(){
        playerHealth = 50;
@@ -99,6 +148,9 @@ var fight = function(enemyName) {
         
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
+        if (playerHealth > 0 && i < enemyNames.length - 1) {
+          shop();
+        }
       }
       // if player isn't alive, stop the game
       else {
